5ee6dcd40c7cc9950292787e74ccdb4bf3d9be62
acz <acz@ABC>
2020-11-09 07:54:46 +0530

    done all comments

--- Action game/Form1.cs
+++ Action game/Form1.cs
@@ -22,45 +22,53 @@ namespace Action_game
 
         private void Actiongame_Load(object sender, EventArgs e)
         {
-            btn_spin.Enabled = false;
-            Btn_Shoot.Enabled = false;
-            btn_ShootAway.Enabled = false;
+            btn_spin.Enabled = false;//disable the spin button//
+            Btn_Shoot.Enabled = false;//disable the shoot button//
+            btn_ShootAway.Enabled = false; //disable the shootaway//
 
         }
 
         private void btn_Load_Click(object sender, EventArgs e)
         {
-            btn_spin.Enabled = true;
-            btn_Load.Enabled = false;
+            btn_spin.Enabled = true;//Enable the spin button//
+            btn_Load.Enabled = false;//disable the Load button//
+
+
+
+
+
+
+
             ;
         }
 
         private void btn_spin_Click(object sender, EventArgs e)
         {
-            Btn_Shoot.Enabled = true;
-            btn_spin.Enabled = false;
+            Btn_Shoot.Enabled = true;//Enable the shoot buuton//
+            btn_spin.Enabled = false;//disable the spin button//
+
         }
 
         private void btn_PA_Click(object sender, EventArgs e)
         {
-            btn_Load.Enabled = true;
-            btn_spin.Enabled = false;
-            Btn_Shoot.Enabled = false;
-            btn_ShootAway.Enabled = false;
+            btn_Load.Enabled = true; //Enable the Load button//
+            btn_spin.Enabled = false;//disable the spin button//
+            Btn_Shoot.Enabled = false;//disable the shoot button//
+            btn_ShootAway.Enabled = false;//disable the shootaway button//
         }
 
         private void btn_ShootAway_Click(object sender, EventArgs e)
         {
-            Btn_Shoot.Enabled = false; 
-            btn_ShootAway.Enabled = false;
-          
+            Btn_Shoot.Enabled = false; //disable the shoot button//
+            btn_ShootAway.Enabled = false;//disable the shootaway button//
+
 
         }
 
         private void Btn_Shoot_Click(object sender, EventArgs e)
         {
-            btn_ShootAway.Enabled = true;
-            Btn_Shoot.Enabled = false;
+            btn_ShootAway.Enabled = true;//disable the shootaway button//
+            Btn_Shoot.Enabled = false;//disable the shoot button//
         }
     }
 }
